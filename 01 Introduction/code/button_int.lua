effbda827c060561aac0be778c1fb5e11147c698
Samuel M.H <samuel.munoz@beeva.com>
2017-06-08 18:27:21 +0200

    nomenclature eased

--- 01 Introduction/code/button_int.lua
+++ 01 Introduction/code/button_int.lua
@@ -4,7 +4,7 @@ pin_button = 2
 gpio.mode(pin_led, gpio.OUTPUT)
 gpio.mode(pin_button, gpio.INT, gpio.PULLUP)
 
-function cb_led(level, when)
+function callback(level, when)
   if level == gpio.HIGH then
     gpio.write(pin_led, gpio.LOW)
   else
@@ -12,4 +12,4 @@ function cb_led(level, when)
   end
 end
 
-gpio.trig(pin_button, "both", cb_led)
+gpio.trig(pin_button, "both", callback)
